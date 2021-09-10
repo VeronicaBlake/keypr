a8ae65b718c0f9191a25b1d5649d9ffdc88f5358
Veronica Blake/Noyce <vrex149@gmail.com>
2021-09-10 12:00:27 -0600

    first commit

---
+++ keypr.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'fbi-surveillance.au.auth0.com'
+export const audience = 'https://keypr'
+export const clientId = '2Karn2SHwDSHPuAi8FSIyOhlCcalynQM'
